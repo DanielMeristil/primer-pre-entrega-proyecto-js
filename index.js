1de3f51444ea767f67d6f822839cd959ce49b5db
unknown <mdshadyacro@gmail.com>
2023-08-13 22:41:15 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,79 @@
+
+for (let z = 8; z <= 18; z++) {
+    for (let y = 8; y <=18; y++){
+        let resultado = z * y;
+        console.log(`${z} * ${y} =
+        ${resultado}`);
+    }
+}
+
+
+
+
+const numeroIngresado = prompt ("Ingrese un numero");
+alert(`Numero $ {numeroIngresado}`);
+
+pedirNumero();
+
+
+function suma(a, b, c, d) {
+ const resultado = a + b + c + d;
+  alert ("EL resultado de la suma es " + resultado);
+}
+
+suma(5, 9, 11, 15);
+
+
+function estacionamientoPrivado ()
+    while(trueOrFalse){
+        const AstonMartin = parseInt(prompt("Ingrese patente"));
+        const McClaren = parseInt(prompt("Ingrese patente"));
+        const Bently = parseInt(prompt("Ingrese patente"));
+        const RollsRoys = parseInt(prompt("Ingrese patente"));
+        const Lamborghini = parseInt(prompt("Ingrese patente"));
+    const estacionamientoPrivado = prompt(
+    "Ingresa numero de patente del auto registrado que bede registrado :\nAston Martin \nMc Claren< \nBently  \nRolls Roys  \nLamborghini  \nsalir"
+     );
+
+     let patente;
+   switch ('ingresa numero patente') {
+   case "P4004":
+    patente = AstonMartin(P4004);
+    break;
+    case "KJ500":
+    patente = McClaren(KJ500);
+    break;
+    case "JP8057":
+    patente = Bently(JP8057);
+    break;
+    case "GC5050":
+    patente = RollsRoys(GC5050);
+    break;
+    case "YZ7089":
+    patente = Lamborghini(YZ7089);
+    break;
+    trueOrFalse = false;
+    break;
+    default:
+    alert("La patente ingresado no se encuentra registrado");
+        break;
+   }
+}
+function AstonMartin(P4004){
+    return P4004;
+}
+function McClaren(KJ500){
+    return KJ500;
+}
+function Bently(JP8057){
+    return JP8057;
+}
+function RollsRoys(GC5050) {
+    return GC5050;
+}
+function Lamborghini(YZ7089){
+    return YZ7089;
+}
+
+const resultado = pa
+console.log(resultado);
